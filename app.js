3e342df99b295a2ba5f7dfb7e53532bb3d3eb776
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-08 12:21:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,20 @@ for (var unitCounter = 0; unitCounter <= canvasWidth; unitCounter++) {
     }
 }
 
+for (var unitCounter = 0; unitCounter <= canvasWidth; unitCounter++) {
+    context.beginPath();
+    context.moveTo(unitCounter, canvasHeight / 2);
+    if (unitCounter % 4 === 0) {
+        context.lineTo(canvasWidth - canvasWidth, canvasHeight - canvasHeight);
+    } else if (unitCounter % 4 === 1) {
+        context.lineTo(canvasWidth, canvasHeight - canvasHeight);
+    } else if (unitCounter % 4 === 2) {
+        context.lineTo(canvasWidth - canvasWidth, canvasHeight);
+    } else if (unitCounter % 4 === 3) {
+        context.lineTo(canvasWidth, canvasHeight);
+    } else {
+        console.log('Something is wrong.');
+    }
+    context.strokeStyle = 'rgba(0,0,255,.5)';
+    context.stroke();
+}
